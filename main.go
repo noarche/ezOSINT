db4aabefb3f30c80746a7520ccaf8edf38b4f452
noarche <n0arch@pm.me>
2026-06-21 23:27:17 -0400

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ var logo = `
 
 var (
 	verbose    bool
-	userAgent  = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3"
+	userAgent  = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15"
 	resultFile = "./ezosint.results.txt"
 )
 
